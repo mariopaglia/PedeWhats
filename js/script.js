d7bc880dc4b0dede0b13050636671ed4461cbe08
Agência H1 Code <mario@h1code.com.br>
2018-11-18 12:35:26 -0200

    Esqueleto inicial do projeto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-﻿
+﻿/**
+ * Verifica se a pizza tem mais de um sabor, se sim, esconde outros campos, se não, libera os campos de "metade 1" e "metade 2"
+ */
+
+$(document).ready(function () {
+    $('#metade1, #metade2').hide();
+    $('input[type=radio][name=mais-sabor]').change(function () {
+        if (this.value == 'sim') {
+            $('#sabor-inteiro-label, #sabor-inteiro').hide();
+            $('#metade1, #metade2').show();
+        } else if (this.value == 'não') {
+            $("#metade1, #metade2").hide();
+            $('#sabor-inteiro-label, #sabor-inteiro').show();
+        }
+    });
+});
+
+/**
+ * Realiza a máscara no campo "telefone da pizzaria"
+ */
+
+$('#telefone').mask('00000000000');
